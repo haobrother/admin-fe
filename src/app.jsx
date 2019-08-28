39f75eb408fe3322b8f45dfe7b218f0d3c7c0a0e
bighaobrother <362571535@qq.com>
2019-08-28 12:53:32 +0800

    initial

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: haobrother 
+ * @Date: 2019-08-27 22:31:12 
+ * @Last Modified by: haobrother
+ * @Last Modified time: 2019-08-28 11:56:10
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+    <div>
+        <h1>Hello, world!</h1>
+    </div>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+/*
+ * @Author: haobrother 
+ * @Date: 2019-08-27 22:30:44 
+ * @Last Modified by: haobrother
+ * @Last Modified time: 2019-08-28 11:57:13
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'js/app.js'
+    },
+    module: {
+        rules: [
+            // react jsx文件的处理
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            },
+            // css文件的处理
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            // sass文件的处理
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            // 图片的处理
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            // 字体图标的配置
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // 处理HTML文件
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        // 独立CSS文件
+        new ExtractTextPlugin("css/[name].css"),
+        // 提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        port: 8086
+    }
+};
